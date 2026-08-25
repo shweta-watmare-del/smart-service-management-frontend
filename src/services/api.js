48eb2fa4d3a03aa91ed57e6abd850a78d89449fa
shweta-watmare-del <v8shweta@gmail.com>
2026-08-25 14:34:12 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:8084/api',
+   const api = axios.create({
+     baseURL: `${import.meta.env.VITE_API_URL || 'http://localhost:8084'}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
